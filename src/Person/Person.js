da582ee1ef3e6a3a74815db6308c8854f59a3545
Kuba Sukowski <kubasulek2@gmail.com>
2019-08-21 14:35:16 +0200

    exercise 1 done

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Person.css';
 
 const person = (props) => {
+
 	return (
 		<div className="Person">
 			<p onClick={props.click} className='person'>I'm a {props.name} and I'm {props.age} years old. </p>
 			<p>
 				{props.children}
 			</p>
-			<input type="text"onChange={props.change} value={props.name}/>
+			<input type="text" onChange={props.change} value={props.name}/>
 		</div>
 	);
 };
