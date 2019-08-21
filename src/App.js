f76e5bb7bb0ecc9d6ab8d0f7e65cd52520da0f54
Kuba Sukowski <kubasulek2@gmail.com>
2019-08-21 19:42:36 +0200

    state array operation in input onchange handler

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
 	state = {
 		persons: [
-			{ name: 'Max', age: 28 },
-			{ name: 'Horse', age: 21 },
-			{ name: 'Boo', age: 2 },
+			{ id: '1', name: 'Max', age: 28 },
+			{ id: '2', name: 'Horse', age: 21 },
+			{ id: '3', name: 'Boo', age: 2 },
 		],
 		showPersons: false
 	}
@@ -21,73 +21,80 @@ class App extends Component {
 		});
 
 	}
-	nameChangedHandler = (event) => {
-		this.setState({
-			persons: [
-				{ name: this.state.persons[0].name, age: this.state.persons[0].age },
-				{ name: event.target.value, age: this.state.persons[1].age },
-				{ name: this.state.persons[2].name, age: this.state.persons[2].age },
-
-			]
-		});
-	}
-	togglePersonsHandler = () => {
-		this.setState({ showPersons: !this.state.showPersons });
-	}
-
-	deletePersonHandler = (personIndex) => {
-		//const persons = this.state.persons.slice(); //shallow copy
-		//const persons = [...this.state.persons];	//shallow copy, also for objects
-		//const persons = Array.from(this.state.persons); //shallow copy
-		const persons = JSON.parse(JSON.stringify(this.state.persons)); // deep copy also for objects
-		persons.splice(personIndex, 1);
+	nameChangedHandler = (id, event) => {
+		
+		const 
+			personIndex = this.state.persons.findIndex(person => person.id === id),
+			person = {...this.state.persons[personIndex]};
+		
+		
+		person.name = event.target.value;
+		
+		const persons = [...this.state.persons];
+		persons[personIndex] = person;
 		this.setState({ persons: persons });
+		console.log(this.state);
+		
 	}
 
-	render() {
+togglePersonsHandler = () => {
+	this.setState({ showPersons: !this.state.showPersons });
+}
 
-		const style = {
-			backgroundColor: 'indianred',
-			padding: '1rem',
-			color: 'white'
-		};
-		let persons = null;
+deletePersonHandler = (personIndex) => {
+	//const persons = this.state.persons.slice(); //shallow copy
+	//const persons = [...this.state.persons];	//shallow copy, also for objects
+	//const persons = Array.from(this.state.persons); //shallow copy
+	const persons = JSON.parse(JSON.stringify(this.state.persons)); // deep copy also for objects
+	persons.splice(personIndex, 1);
+	this.setState({ persons: persons });
+}
 
-		if (this.state.showPersons) {
-			persons = (
-				<div>
-					{this.state.persons.map((person, index) => {
-						return (
-							<Person
-								key={index + 1}
-								click={() => this.deletePersonHandler(index)}
-								name={person.name}
-								age={person.age}
-							/>
-						);
-					})}
-				</div>);
-		}
+render() {
+
+	const style = {
+		backgroundColor: 'indianred',
+		padding: '1rem',
+		color: 'white'
+	};
+	let persons = null;
+
+	if (this.state.showPersons) {
+		persons = (
+			<div>
+				{this.state.persons.map((person, index) => {
+					return (
+						<Person
+							change={this.nameChangedHandler.bind(this, person.id)}
+							key={person.id}
+							click={() => this.deletePersonHandler(index)}
+							name={person.name}
+							age={person.age}
+						/>
+					);
+				})}
+			</div>);
+	}
 
 
 
-		return (
-			<div className="App">
-				<h1>Hi,  I'm React App</h1>
-				<p>this is really working</p>
+	return (
+		<div className="App">
+			<h1>Hi,  I'm React App</h1>
+			<p>this is really working</p>
 
-				<button
-					style={style}
-					onClick={this.togglePersonsHandler}
-				>
-					Switch Name
+			<button
+				style={style}
+				onClick={this.togglePersonsHandler}
+			>
+				Switch Name
 				</button>
 
-				{persons}
+			{persons}
 
-			</div>
-		);
-	}
+		</div>
+	);
+}
 }
 
 export default App;
